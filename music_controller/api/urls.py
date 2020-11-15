4e70d09eecf9970d67295aa3982fc95f2bed3686
iampaavan <paavan014@gmail.com>
2020-11-14 23:46:18 -0800

    Room Serializer

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import main, RoomView
  
 urlpatterns = [
-    path('', main)
+    path('home', main),
+    path('', main),
+    path('view', RoomView.as_view())
 ]
